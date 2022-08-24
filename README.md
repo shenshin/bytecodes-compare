bfaa09c07de0c24eb17f12a3c9d54744d4985dff
Aleksandr Shenshin <shenshin@me.com>
2022-08-24 18:33:03 +0200

    compare using ethers.js / solcjs only

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # Compare RSK Testnet vs SolcJS bytecodes
 
 This project demonstrates how to:
-- deploy `Demo` smart contract to RSK Testnet
-- compile the same smart contract with `SolcJS` version `0.8.9`
+- compile the `Demo` smart contract with `SolcJS` version `0.8.7`
+- deploy the compiled bytecode to RSK Testnet using `ethers.js`
 - compare the bytecode on RSK Testnet and the one comming from `SolcJS`
+- make sure they are identical
 
 ## Installation
 Rename `template.secret.json` to `.secret.json` and paste your seed phrase for generating RSK accounts
@@ -14,5 +15,5 @@ npm install
 
 ## Compile smart contract, Deploy to RSK and Compare bytecodes
 ```shell
-npm run compare
+./compare.js
 ```
--- compare.js
+++ compare.js
@@ -1,13 +1,22 @@
-const hre = require('hardhat');
+#! /usr/bin/env node
+
+const ethers = require('ethers');
 const solc = require('solc');
 const { resolve } = require('path');
 const { readFile } = require('fs/promises');
+const { mnemonic } = require('./.secret.json');
 
 async function main() {
   try {
+    // connecting to JSON RPC provider and setting up wallet
+    const provider = ethers.getDefaultProvider(
+      'https://public-node.testnet.rsk.co/',
+    );
+    const deployer = ethers.Wallet.fromMnemonic(mnemonic).connect(provider);
+
     // read the smart contract source file
     const demoSolidity = await readFile(
-      resolve(__dirname, '..', 'contracts', 'Demo.sol'),
+      resolve(__dirname, 'contracts', 'Demo.sol'),
       'utf8',
     );
 
@@ -41,22 +50,18 @@ async function main() {
     const output = JSON.parse(solc.compile(JSON.stringify(input)));
     const demoOutput = output.contracts['Demo.sol'].Demo;
 
-    // deploying to RSK using the bytecode gererated by SolcJS (not Hardhat!!!)
-    const [deployer] = await hre.ethers.getSigners();
-    const ContractFactory = hre.ethers.ContractFactory.fromSolidity(
+    // deploying to RSK using the bytecode gererated by SolcJS
+    const ContractFactory = ethers.ContractFactory.fromSolidity(
       demoOutput,
       deployer,
     );
+    console.log(`Deploying Demo smart contract to RSK Testnet`);
     const demoSC = await ContractFactory.deploy();
     await demoSC.deployed();
-    console.log(
-      `Deployed to ${hre.network.name} with address ${demoSC.address}`,
-    );
+    console.log(`Deployed Demo to RSK Testnet with address ${demoSC.address}`);
 
     // read bytecode from RSK Testnet
-    const rskByteCode = (
-      await hre.ethers.provider.getCode(demoSC.address)
-    ).substring(2);
+    const rskByteCode = (await provider.getCode(demoSC.address)).substring(2);
 
     // compare RSK and Solc bytecodes
     const solcDeployedBytecode = demoOutput.evm.deployedBytecode.object;
@@ -71,9 +76,7 @@ async function main() {
         : `Bytecodes are different`,
     );
   } catch (error) {
-    console.error(error);
-    process.exitCode = 1;
+    console.log(error);
   }
 }
-
 main();
